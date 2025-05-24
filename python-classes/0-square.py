61eefdac80db09301001171bc46b1034c678716c
donnasaud <donnaalmadani@gmail.com>
2025-05-24 18:56:21 +0000

    task 0 fix

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -8,10 +8,3 @@ class Square:
     Represents an empty square.
     """
     pass
-
-
-# Test code
-if __name__ == "__main__":
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
