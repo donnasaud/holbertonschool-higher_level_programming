6797c61e3a7b9c5f16d3539a48a7fff7742ed69a
donnasaud <donnaalmadani@gmail.com>
2025-05-24 21:41:06 +0000

    task 5 in classes

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """Returns a new string with all 'c' and 'C' characters removed."""
+    return "".join(char for char in my_string if char != 'c' and char != 'C')
