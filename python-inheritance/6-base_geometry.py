1119da02d1b486af9559709e0b32d6f492eab779
donnasaud <donnaalmadani@gmail.com>
2025-05-31 20:37:10 +0000

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a BaseGeometry class with an area method placeholder.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry defines a base class for geometry operations.
+    """
+
+    def area(self):
+        """
+        Raises an Exception to indicate area is not implemented.
+        """
+        raise Exception("area() is not implemented")
