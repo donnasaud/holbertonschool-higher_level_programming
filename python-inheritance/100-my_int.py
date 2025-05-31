0c66ea91e146ed3518da51f93f5ccf1ab427a68b
donnasaud <donnaalmadani@gmail.com>
2025-05-31 21:16:36 +0000

    task 100

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""MyInt is a rebel that inverts == and !="""
+
+class MyInt(int):
+    """A subclass of int with inverted equality operators"""
+
+    def __eq__(self, other):
+        """Invert the == operator"""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Invert the != operator"""
+        return super().__eq__(other)
