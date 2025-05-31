a8ceb07ff99b5475a58c6e6a3c90a18bc1d73b27
donnasaud <donnaalmadani@gmail.com>
2025-05-31 20:35:24 +0000

    task 5 in python

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class named BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class for geometry-related operations.
+    """
+    pass
