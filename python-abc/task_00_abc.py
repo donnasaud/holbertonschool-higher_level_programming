5242fd62db3cf0077931f9107d65098a1006e314
donnasaud <donnaalmadani@gmail.com>
2025-06-01 20:20:51 +0000

    animal class task 0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        """Abstract method to be implemented by subclasses"""
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
