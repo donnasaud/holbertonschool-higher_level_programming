4b24a2ae299488c7e51039d84413cd3234ba4542
donnasaud <donnaalmadani@gmail.com>
2025-06-22 15:57:39 +0000

    fixing a line

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read and print the contents of a UTF-8 text file.
+"""
 
 def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
+
