eedeef8e2b620ca454d775b914a9aee4560d7a56
donnasaud <donnaalmadani@gmail.com>
2025-08-02 12:20:19 +0000

    task 2 fixes

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
-"""Module that defines LockedClass with restricted attribute creation."""
 #!/usr/bin/python3
+
+"""Module that defines LockedClass with restricted attribute creation."""
+
+
 class LockedClass:
     """Prevents dynamic attributes except 'first_name'."""
     __slots__ = ["first_name"]
