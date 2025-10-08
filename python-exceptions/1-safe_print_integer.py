fc40b80b59dcd3d23d7f20db21ea93206ee365e5
donnasaud <donnaalmadani@gmail.com>
2025-10-08 21:02:52 +0000

    task 0 till task 6

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        value + 1
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
