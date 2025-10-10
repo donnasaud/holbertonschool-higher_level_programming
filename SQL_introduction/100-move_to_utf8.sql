850b7a4613cb309401c1a3f8db19f3f89b29efc2
donnasaud <donnaalmadani@gmail.com>
2025-10-10 11:34:34 +0000

    advanced task fixes

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,24 +1,14 @@
--- 100-move_to_utf8.sql
--- Convert database, table, and 'name' column to UTF8 (utf8mb4 / utf8mb4_unicode_ci)
--- Ensure the 'name' column DOES NOT show 'CHARACTER SET' in SHOW CREATE TABLE.
+-- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci) in your MySQL server
 
--- 1) Set database defaults
+-- Converts the database to UTF8
 ALTER DATABASE hbtn_0c_0
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
--- 2) Use the database
+-- Selects a database to use
 USE hbtn_0c_0;
 
--- 3) Convert the whole table to utf8mb4/unicode_ci
+-- Converts the table to UTF8
 ALTER TABLE first_table
-  CONVERT TO CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
-
--- 4) Change column: remove explicit CHARACTER SET from the definition
--- This makes MySQL inherit the table/default charset for the column.
-ALTER TABLE first_table
-  MODIFY name VARCHAR(256)
-  COLLATE utf8mb4_unicode_ci
-  DEFAULT NULL;
-
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
