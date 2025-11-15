d7c548b842a59c0c1d5e1a8b6a12ff18e499e032
donnasaud <donnaalmadani@gmail.com>
2025-11-15 11:51:06 +0000

    ad task 3

---
+++ javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+myObject.incr = function () {
+  this.value += 1;
+};
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
